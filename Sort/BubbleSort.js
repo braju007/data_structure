f310dc53a3a17f8f5a0f093f3b5a631e30769d17
braju007 <erbrj7@gmail.com>
2025-08-23 19:56:14 +0530

    Run prettier package

--- Sort/BubbleSort.js
+++ Sort/BubbleSort.js
@@ -1,14 +1,14 @@
 function bubbleSort(arr) {
-    let swapped;
-    do {
-        swapped = false;
-        for (let i = 0; i < arr.length - 1; i++) {
-            if (arr[i] > arr[i + 1]) {
-                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
-                swapped = true
-            }
-        }
-    } while (swapped);
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
+      }
+    }
+  } while (swapped);
 }
 const arr = [10, 8, 7, -5, -2, 0];
 bubbleSort(arr);
--- Sort/QuickSort.js
+++ Sort/QuickSort.js
@@ -1,21 +1,20 @@
-function quickSort(arr){
-    if(arr.length <=1){
-      return arr;
-    }
-    let pivot = arr[0];
-    const left = [];
-    const right = [];
-    for(let i=1; i<arr.length;i++){
-      if(arr[i]<pivot){
-        left.push(arr[i]);
-      }else{
-        right.push(arr[i])
-      }
-    }
-    return [...quickSort(left),pivot,...quickSort(right)]
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
   }
-  
-  const array = [5,3,7,9,6,1,0];
-  console.log("Quick :", quickSort(array));
-  // [0, 1, 3, 5, 6, 7, 9]
-  
+  let pivot = arr[0];
+  const left = [];
+  const right = [];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+const array = [5, 3, 7, 9, 6, 1, 0];
+console.log("Quick :", quickSort(array));
+// [0, 1, 3, 5, 6, 7, 9]
